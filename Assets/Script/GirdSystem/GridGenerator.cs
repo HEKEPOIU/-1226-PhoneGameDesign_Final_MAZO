4229f546f4dc3b0e3da706454febb87da79f5726
HEKEPOIU <decnovpk92805@gmail.com>
2023-12-30 22:31:18 +0800

    Add Gizmos to visualize grid

--- Assets/Script/GirdSystem/GridGenerator.cs
+++ Assets/Script/GirdSystem/GridGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Singleton;
+﻿using System;
+using Singleton;
+using UnityEditor;
 using UnityEngine;
 
 namespace GirdSystem
@@ -8,6 +10,8 @@ namespace GirdSystem
         [SerializeField] private Vector3 _originPosition;
         [SerializeField] private Vector2Int _gridSize;
         [SerializeField] private float _cellSize = 10f;
+        [SerializeField] private bool _isGridVisualize = true;
+        [SerializeField] private bool _isGridLabelsVisualize = true;
         public BaseGrid<IGridObject> Grid { get; private set; }
 
         protected override void Awake()
@@ -15,6 +19,32 @@ namespace GirdSystem
             base.Awake();
             Grid = new BaseGrid<IGridObject>(_gridSize.x, _gridSize.y, _cellSize, _originPosition);
         
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (_isGridVisualize == false) return;
+
+            
+            Handles.color = Color.red;
+
+            for (int x = 0; x < _gridSize.x; x++)
+            {
+                for (int y = 0; y < _gridSize.y; y++)
+                {
+                    Handles.DrawLine(new Vector3(x, y) * _cellSize + _originPosition, 
+                        new Vector3(x, y+1) * _cellSize + _originPosition);
+                    Handles.DrawLine(new Vector3(x, y) * _cellSize + _originPosition, 
+                        new Vector3(x+1, y) * _cellSize + _originPosition);
+                    if (!_isGridLabelsVisualize) continue;
+
+                    Handles.Label(new Vector3(x, y) * _cellSize + _originPosition + new Vector3(_cellSize, _cellSize) * .5f, x + "," + y);
+                }
+            }
+            Handles.DrawLine(new Vector3(0, _gridSize.y) * _cellSize + _originPosition,
+                new Vector3(_gridSize.x, _gridSize.y) * _cellSize + _originPosition);
+            Handles.DrawLine(new Vector3(_gridSize.x, 0) * _cellSize + _originPosition, 
+                new Vector3(_gridSize.x, _gridSize.y) * _cellSize + _originPosition);
         }
     }
 }
