b9b81e1fd18ae4458a4999a70d896f2610c3690c
HEKEPOIU <decnovpk92805@gmail.com>
2024-01-02 07:01:30 +0800

    add Feel Package

---
+++ Assets/Script/Character/RockGridCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+    public enum RockDirection
+    {
+        Left,
+        Right
+    }
+    public class RockGridCharacter : ItemGridCharacter
+    {
+        public RockDirection RockDirection;
+    }
+}
